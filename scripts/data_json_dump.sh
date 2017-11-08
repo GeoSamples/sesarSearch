7ec91e84a544fa7499586138f009da694dbb6564
Lulin Song <song@stats-dev.ldeo.columbia.edu>
2017-11-08 15:42:29 -0500

    Updated dir name and added document

--- scripts/data_json_dump.sh
+++ scripts/data_json_dump.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+#
+# data_json_dump.sh
+#
+# This script will create JSON file for each IGSN listed in a text file.
+#
+
+#Loop through each IGSN in igsnlist_300.csv file
 for i in $(cat igsnlist_300.csv); do
-   echo "${i}"
-   curl -X GET -H "accept: application/json" "https://app.geosamples.org/webservices/display.php?igsn=${i} -L" > json/${i}.json
+   #echo "${i}"
+   #Call SESAR web service to create JSON file for each IGSN
+   curl -s -X GET -H "accept: application/json" "https://app.geosamples.org/webservices/display.php?igsn=${i} -L" > ../json/${i}.json
 done 
